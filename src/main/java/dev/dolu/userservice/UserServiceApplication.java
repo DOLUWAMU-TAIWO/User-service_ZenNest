b0f2c7b339234fb34ac4630c49a4ff18adb8b049
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2025-03-05 20:11:26 +0100

    Added config for restemplate 5 seconds timer for alle rrors

--- src/main/java/dev/dolu/userservice/UserServiceApplication.java
+++ src/main/java/dev/dolu/userservice/UserServiceApplication.java
@@ -10,8 +10,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import java.util.List;
 
 import static dev.dolu.userservice.models.Role.ADMIN;
@@ -27,7 +30,10 @@ public class UserServiceApplication {
     }
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000); // 5 seconds connect timeout
+        requestFactory.setReadTimeout(5000);    // 5 seconds read timeout
+        return new RestTemplate(requestFactory);
     }
 
 }
