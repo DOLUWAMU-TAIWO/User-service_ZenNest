343641349caa37a1677a3d0c2517a4ae8fdfa6c0
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2025-05-05 10:52:57 +0200

    Tempalte issue fixed cors working at local dev 7500

--- src/main/java/dev/dolu/userservice/UserServiceApplication.java
+++ src/main/java/dev/dolu/userservice/UserServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -16,9 +18,15 @@ public class UserServiceApplication {
     }
 
     @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5_000); // 5 seconds
+        requestFactory.setReadTimeout(5_000);
+        return new RestTemplate(requestFactory);
+    }
+
+    @Bean
     public CommandLineRunner startupMessage() {
-        return args -> {
-            log.info("🚀 UserService is running in Docker Compose [PRODUCTION BUILD]");
-        };
+        return args -> log.info("🚀 UserService is running in Docker Compose [PRODUCTION BUILD]");
     }
 }
